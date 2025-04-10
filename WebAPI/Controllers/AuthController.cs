db41a50ba18f77465d30fc32bb53e562ba05a10d
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-10 23:14:09 +0630

    [Add] AuthController for user authentication

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Contract;
+using Application.Models.Login;
+using Application.Models.Register;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUser _user;
+
+        public AuthController(IUser user)
+        {
+            _user = user;
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<LoginResponseModel>> UserLogin(LoginUserModel loginUser)
+        {
+           var result = await _user.LoginUserAsync(loginUser);
+            if (result.Flag)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<RegisterResponseModel>> UserRegister(RegisterUserModel registerUser)
+        {
+            var result = await _user.RegisterUserAsync(registerUser);
+            if (result.Flag)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+    }
+}
