5439005c0eb4ec49e8984698790c9d2aadf7f84b
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-10 23:14:48 +0630

    [Add] authentication and repository services

--- WebAPI/ModularServices.cs
+++ WebAPI/ModularServices.cs
@@ -1,22 +1,57 @@
-﻿using Infrastructure.Data;
+﻿using Application.Contract;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Infrastructure.Repo;
 namespace WebAPI
 {
     public static class ModularServices
     {
-        public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
+        public static IServiceCollection AddServices(this IServiceCollection services, 
+            WebApplicationBuilder builder, IConfiguration configuration)
         {
-            return services.AddDbContext(builder);
+            services
+                .AddDbContext(builder)
+                .AddAuthentication(builder, configuration)
+                .AddRepo();
+            return services;
         }
 
         public static IServiceCollection AddDbContext(this IServiceCollection services, WebApplicationBuilder builder)
         {
             var connectionString = builder.Configuration.GetConnectionString("DbConnection");
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(connectionString), 
-                ServiceLifetime.Transient, 
+                options.UseSqlServer(connectionString),
+                ServiceLifetime.Transient,
                 ServiceLifetime.Transient);
+            return services;
+        }
+
+        public static IServiceCollection AddAuthentication(this IServiceCollection services, 
+            WebApplicationBuilder builder, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["JWT:Issuer"],
+                ValidAudience = builder.Configuration["JWT:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]!))
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddRepo(this IServiceCollection services)
+        {
+            services.AddScoped<IUser, UserRepo>();
             return services;
         }
     }
