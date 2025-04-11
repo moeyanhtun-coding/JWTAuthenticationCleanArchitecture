c0a523ed24e21c22a32f2cc97dbbec4b9b36ae8f
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 23:16:43 +0630

    [Add] CustomAuthenticationStateProvider for auth management

---
+++ BlazorWasm/State/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorWasm.State
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private const string LocalStorageKey = "auth";
+        private readonly ILocalStorageService localStorageService;
+        private readonly ClaimsPrincipal anonymous = new (new ClaimsIdentity());
+        public CustomAuthenticationStateProvider(ILocalStorageService localStorageService)
+        {
+            this.localStorageService = localStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = await localStorageService.GetItemAsStringAsync(LocalStorageKey);
+            if(string.IsNullOrEmpty(token)) return await Task.FromResult(new AuthenticationState(anonymous));
+        }
+    }
+}
