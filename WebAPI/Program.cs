ea94c533685dae12ad8d5c0cba396d30e5460fca
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 08:11:58 +0630

    [Add] Swagger support with JWT authentication

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using WebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,41 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    swagger =>
+    {
+        swagger.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "ASP.NET Core 8 Web API",
+            Description = "ASP.NET Core 8 Web API with JWT Authentication"
+        });
+        swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    }
+    ); 
+
 builder.Services.AddServices(builder, builder.Configuration);
 
 var app = builder.Build();
