6afc729630419a07ac35861aedd3eee687a7277e
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 23:16:15 +0630

    [Implement] user authentication in AccountService

---
+++ Application/Services/Authentication/AccountService.cs
@@ -0,0 +1,35 @@
+﻿using Application.Models.Login;
+using Application.Models.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Authentication
+{
+    public class AccountService : IAccount
+    {
+        private readonly HttpClient httpClient;
+
+        public AccountService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<LoginResponseModel> LoginAccountAsync(LoginUserModel model)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/auth/login", model);
+            var result = await response.Content.ReadFromJsonAsync<LoginResponseModel>();
+            return result!;
+        }
+
+        public async Task<RegisterResponseModel> RegisterAccountAsync(RegisterUserModel model)
+        {
+           var response = await httpClient.PostAsJsonAsync("api/auth/register", model);
+            var result = await response.Content.ReadFromJsonAsync<RegisterResponseModel>();
+            return result!;
+        }
+    }
+}
---
+++ Application/Services/Authentication/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using Application.Models.Login;
+using Application.Models.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Authentication
+{
+    public interface IAccount
+    {
+        Task<RegisterResponseModel> RegisterAccountAsync(RegisterUserModel model);
+        Task<LoginResponseModel> LoginAccountAsync(LoginUserModel model);
+    }
+}
