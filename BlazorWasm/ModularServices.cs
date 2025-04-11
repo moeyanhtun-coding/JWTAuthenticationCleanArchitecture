4b29e1cfe93d3812b074f0a51f3708ff2f0b33f9
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-11 23:17:08 +0630

    [Add] ModularServices for service registration

---
+++ BlazorWasm/ModularServices.cs
@@ -0,0 +1,15 @@
+﻿using Application.Services.Authentication;
+using Blazored.LocalStorage;
+
+namespace BlazorWasm
+{
+    public static class ModularServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services) 
+        {
+            services.AddScoped<IAccount, AccountService>();
+            services.AddBlazoredLocalStorage();
+            return services;
+        }
+    }
+}
